7d486c21128d716e01498e784b0e865fd5f119a5
salemi <en.salemi75@gmail.com>
2023-04-18 08:41:22 +0330

    feat: create fast food item component

---
+++ src/components/FastFoodItem/index.tsx
@@ -0,0 +1,29 @@
+import { HiShoppingCart } from "react-icons/hi";
+
+import "./fastFoodItem.css";
+import { FastFoodItemProps } from "./type";
+
+const FastFoodItem = (props: FastFoodItemProps) => {
+  const { name, price, ingredients, imageUrl } = props;
+
+  return (
+    <div className="card product-card h-100 border-0 shadow-sm pb-1">
+      <span className="badge badge-end badge-shadow bg-success fs-md fw-medium">
+        قیمت: {price.toLocaleString()} تومان
+      </span>
+      <div className="card__placeholder">
+        <img className="card-img-top" src={imageUrl} alt={name} />
+      </div>
+      <div className="card-body text-center pt-3 pb-4 d-flex flex-column">
+        <h5 className="mb-2">{name}</h5>
+      </div>
+      <div className="fs-m fw-bold text-muted mb-3">{ingredients}</div>
+      <button className="btn btn-outline-success btn-sm w-100 mt-auto fw-bold">
+        <HiShoppingCart className="fs-5 ms-3" />
+        افزودن به سبد خرید
+      </button>
+    </div>
+  );
+};
+
+export default FastFoodItem;
---
+++ src/components/FastFoodItem/type.ts
@@ -0,0 +1,6 @@
+export interface FastFoodItemProps {
+  name: string;
+  price: number;
+  ingredients: string;
+  imageUrl: string;
+}
